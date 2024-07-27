0a05df2231a4a4e195d1ba3b859c3da2526ebeb6
AdarshRawat1 <AdarshRawat1@users.noreply.github.com>
2024-07-27 10:37:05 +0000

    Deploying to gh-pages from @ AdarshRawat1/P4c-Documentation-Hosting@0d5eec3ac3b7effa694c7d709dda609e382246a6 🚀

---
+++ pr-preview/9/search/pages_d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['standard_0',['Coding Standard',['../md__2home_2runner_2work_2_p4c-_documentation-_hosting_2_p4c-_documentation-_hosting_2docs_2_coding_standard_philosophy.html',1,'']]]
+];
