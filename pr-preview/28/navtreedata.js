80ebf712f47b785ab7358035b0ef434583413642
AdarshRawat1 <AdarshRawat1@users.noreply.github.com>
2024-08-17 18:31:49 +0000

    Deploying to gh-pages from @ AdarshRawat1/P4c-Documentation-Hosting@ac9bc19fe7a892b735f6a23a68dc349bd68bfd42 🚀

---
+++ pr-preview/28/navtreedata.js
@@ -0,0 +1,92 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "P4 Compiler Documentation (P4C)", "index.html", [
+    [ "Backends", "usergroup0.html", [
+      [ "Behavioral Model Backend", "behavioral_model_backend.html", null ],
+      [ "DPDK Backend", "dpdk_backend.html", null ],
+      [ "eBPF Backend", "ebpf_backend.html", null ],
+      [ "TC Backend", "tc_backend.html", null ],
+      [ "uBPF Backend", "ubpf_backend.html", null ],
+      [ "P4test Backend", "p4test_backend.html", null ],
+      [ "Graphs Backend", "graphs_backend.html", null ]
+    ] ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"class_p4_1_1_b_m_v2_1_1_pna_nic_backend.html",
+"class_p4_1_1_control_plane_a_p_i_1_1_p4_info_maps.html#ae66c42a97f9980ced4fdb8c3cc6f4f8e",
+"class_p4_1_1_d_p_d_k_1_1_direction_to_reg_read.html#a13185144f9542dd94b567f316f0d088e",
+"class_p4_1_1_do_convert_errors.html",
+"class_p4_1_1_e_b_p_f_1_1_e_b_p_f_hash_p_s_a.html#a04c71d4e53f8536089d0b1079c57c9ac",
+"class_p4_1_1_e_b_p_f_1_1_x_d_p_egress_deparser_p_s_a.html",
+"class_p4_1_1_if_in_parser.html",
+"class_p4_1_1_p4_tools_1_1_abstract_p4c_tool.html#abb583f3714214154261c44ee9ed33f3e",
+"class_p4_1_1_p4_tools_1_1_p4_testgen_1_1_bmv2_1_1_bmv2_test_backend.html#add367b59dc9dc5a8473a12e271a01028",
+"class_p4_1_1_p4_tools_1_1_p4_testgen_1_1_bmv2_1_1_token.html#a97ab9a18c1613a98438b5a554f6cacbe",
+"class_p4_1_1_p4_tools_1_1_p4_testgen_1_1_expr_stepper.html#a26eac5191fab531d8b02ed28201a99be",
+"class_p4_1_1_p4_tools_1_1_p4_testgen_1_1_pna_1_1_shared_pna_program_info.html#aeb2ee22471989aa047f509c4d7c6d1a9",
+"class_p4_1_1_p4_tools_1_1_p4_testgen_1_1_test_framework.html#abc35f8e3e43c46a81b8111aee85b5577",
+"class_p4_1_1_p4_tools_1_1_trace_events_1_1_packet.html",
+"class_p4_1_1_pass_manager_1_1_visitor_ref.html",
+"class_p4_1_1_simplify_select_cases.html",
+"class_p4_1_1_test_1_1_z3_solver_test.html",
+"class_p4_1_1_validate_parsed_program.html",
+"functions_r.html",
+"namespace_p4_1_1_p4_tools_1_1_p4_testgen_1_1_pna.html",
+"struct_p4_1_1_diagnostic_count_info_state.html",
+"struct_p4_1_1_specialization_info.html#a6b31fa3c61b974d54e51cba63e5d8554"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ pr-preview/28/search/pages_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['repository_20organization_0',['P4C Repository Organization',['../repository_structure.html',1,'']]],
+  ['representation_20ir_1',['P4C Intermediate Representation (IR)',['../intermediate_representation_ir.html',1,'']]]
+];
